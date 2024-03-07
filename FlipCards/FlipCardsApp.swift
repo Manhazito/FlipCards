8900229ee2e36abb282da2e0f67334bee7638f42
Filipe Ramos <pandinus.imperator@gmail.com>
2024-03-07 13:24:12 +0000

    First commit

---
+++ FlipCards/FlipCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipCardsApp.swift
+//  FlipCards
+//
+//  Created by Filipe Ramos on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlipCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
